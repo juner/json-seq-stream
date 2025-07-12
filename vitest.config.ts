d39d6610fb2aa0ea87907df2795db1d5191b36f3
juner <juner@juner.net>
2025-07-12 12:19:38 +0900

    add githubaction vitest coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, coverageConfigDefaults } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      reporter: ["cobertura", "text"],
+      reporter: ["cobertura", "text", "json-summary", "json"],
       reportsDirectory: "./TestResults/coverage",
       exclude: [
         ...coverageConfigDefaults.exclude,
