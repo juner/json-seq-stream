71f68123b3a2a1641904bb29fe324ac27174c35f
juner <juner@juner.net>
2025-07-13 17:08:04 +0900

    add editorconfig / format

--- src/InputJsonSequenceParseStream.ts
+++ src/InputJsonSequenceParseStream.ts
@@ -2,52 +2,52 @@
 import type { TransformStreamConstructor } from "./TransformStreamConstructor";
 
 export type InputJsonSequenceParseStreamOptions<T> = {
-    parse?: (text: string) => (T | undefined),
-    errorFallback?: false | ((
-        error: unknown, args: {
-            error: (reason: unknown) => void,
-            chunk: string,
-            enqueue: (arg: T) => void
-        }) => (void | Promise<void>));
+  parse?: (text: string) => (T | undefined),
+  errorFallback?: false | ((
+    error: unknown, args: {
+      error: (reason: unknown) => void,
+      chunk: string,
+      enqueue: (arg: T) => void
+    }) => (void | Promise<void>));
 };
 
 
 function makeInternalInputJsonSequenceParseStream<T>({ parse, errorFallback }: InputJsonSequenceParseStreamOptions<T> = {}): {
-    args: ConstructorParameters<TransformStreamConstructor<string, T>>
+  args: ConstructorParameters<TransformStreamConstructor<string, T>>
 } {
-    parse ??= JSON.parse;
-    errorFallback ??= false;
-    const args: ConstructorParameters<TransformStreamConstructor<string, T>> = [
-        {
-            transform(chunk, controller) {
-                try {
-                    const result = parse(chunk);
-                    if (result) controller.enqueue(result);
-                } catch (e: unknown) {
-                    if (!errorFallback) return;
-                    const error = controller.error.bind(controller);
-                    const enqueue = controller.enqueue.bind(controller);
-                    const reuslt = errorFallback(e, { chunk, error, enqueue });
-                    if (reuslt?.then)
-                        return reuslt.catch(() => undefined);
-                }
-            }
+  parse ??= JSON.parse;
+  errorFallback ??= false;
+  const args: ConstructorParameters<TransformStreamConstructor<string, T>> = [
+    {
+      transform(chunk, controller) {
+        try {
+          const result = parse(chunk);
+          if (result) controller.enqueue(result);
+        } catch (e: unknown) {
+          if (!errorFallback) return;
+          const error = controller.error.bind(controller);
+          const enqueue = controller.enqueue.bind(controller);
+          const reuslt = errorFallback(e, { chunk, error, enqueue });
+          if (reuslt?.then)
+            return reuslt.catch(() => undefined);
         }
-    ];
-    return { args };
+      }
+    }
+  ];
+  return { args };
 }
 
 /**
  * string の sequence を 連続した `T` の sequence に変換する TransformStream
  */
 export class InputJsonSequenceParseStream<T> extends TransformStream<string, T> {
-    constructor(options: InputJsonSequenceParseStreamOptions<T> = {}) {
-        const { args } = makeInternalInputJsonSequenceParseStream(options);
-        super(...args);
-    }
+  constructor(options: InputJsonSequenceParseStreamOptions<T> = {}) {
+    const { args } = makeInternalInputJsonSequenceParseStream(options);
+    super(...args);
+  }
 }
 
-// #region deprecated remove v2.0.0 - 
+// #region deprecated remove v2.0.0 -
 
 /**
  * @deprecated rename to InputJsonSequenceFormatStreamOptions
--- src/InputSequenceStream.ts
+++ src/InputSequenceStream.ts
@@ -4,55 +4,55 @@ const LINE_BEGIN = '\u00e1';
 const LINE_END = '\n';
 
 export type InputSequenceStreamOptions = {
-    lineBegin?: string;
-    lineEnd?: string;
+  lineBegin?: string;
+  lineEnd?: string;
 };
 function makeInternalInputSequenceStream({ lineBegin, lineEnd }: InputSequenceStreamOptions = {}): {
-    args: ConstructorParameters<TransformStreamConstructor<string, string>>
+  args: ConstructorParameters<TransformStreamConstructor<string, string>>
 } {
-    lineBegin ??= LINE_BEGIN;
-    lineEnd ??= LINE_END;
-    const sequence: string[] = [];
-    const separater = new RegExp(`(${RegExp.escape(lineBegin)}|${RegExp.escape(lineEnd)})`, "u");
-    const args: ConstructorParameters<TransformStreamConstructor<string, string>> = [
-        {
-            transform(chunk: string, controller: TransformStreamDefaultController<string>) {
-                for (const c of chunk.split(separater)) {
-                    if (c === lineBegin) {
-                        const enqueue = enqueueSequence(sequence);
-                        if (enqueue.length <= 0) continue;
-                        controller.enqueue(enqueue);
-                        continue;
-                    }
-                    if (c === lineEnd) {
-                        continue;
-                    }
-                    sequence.push(c);
-                }
-            },
-            flush(controller: TransformStreamDefaultController<string>) {
-                const enqueue = enqueueSequence(sequence);
-                if (enqueue.length <= 0) return;
-                controller.enqueue(enqueue);
-            }
+  lineBegin ??= LINE_BEGIN;
+  lineEnd ??= LINE_END;
+  const sequence: string[] = [];
+  const separater = new RegExp(`(${RegExp.escape(lineBegin)}|${RegExp.escape(lineEnd)})`, "u");
+  const args: ConstructorParameters<TransformStreamConstructor<string, string>> = [
+    {
+      transform(chunk: string, controller: TransformStreamDefaultController<string>) {
+        for (const c of chunk.split(separater)) {
+          if (c === lineBegin) {
+            const enqueue = enqueueSequence(sequence);
+            if (enqueue.length <= 0) continue;
+            controller.enqueue(enqueue);
+            continue;
+          }
+          if (c === lineEnd) {
+            continue;
+          }
+          sequence.push(c);
         }
-    ];
-    return {
-        args,
-    };
-
-    function enqueueSequence(sequence: string[]) {
-        return sequence.splice(0, sequence.length).join("");
+      },
+      flush(controller: TransformStreamDefaultController<string>) {
+        const enqueue = enqueueSequence(sequence);
+        if (enqueue.length <= 0) return;
+        controller.enqueue(enqueue);
+      }
     }
+  ];
+  return {
+    args,
+  };
+
+  function enqueueSequence(sequence: string[]) {
+    return sequence.splice(0, sequence.length).join("");
+  }
 }
 /**
  * RS で始まって LF で1行を表す 連続した文字列データを 行データとして整形して返す
  */
 export class InputSequenceStream extends TransformStream<string, string> {
-    constructor(options?: InputSequenceStreamOptions) {
-        const { args } = makeInternalInputSequenceStream(options);
-        super(...args);
-    }
+  constructor(options?: InputSequenceStreamOptions) {
+    const { args } = makeInternalInputSequenceStream(options);
+    super(...args);
+  }
 }
 
 // #region deprecated remove v2.0.0 -
