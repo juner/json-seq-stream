2a402739ee0b8c002a23d6c5530e9c8a01edd56e
juner <juner@juner.net>
2025-07-17 18:15:42 +0900

    options の型を定義

--- src/InputLineFeedSeparattedSequenceStream.ts
+++ src/InputLineFeedSeparattedSequenceStream.ts
@@ -1,13 +1,15 @@
 import { LF, CRLF } from "./jsonlines";
 import { TextSplitStream, TextSplitStreamOptions } from "./TextSplitStream";
 
+export type InputLineFeedSeparattedSequenceStreamOptions = Partial<TextSplitStreamOptions>;
+
 const LINEFEED_SEPARATOR = [LF, CRLF];
 /**
  * Stream to convert to LF or CRLF delimited sequence
  */
 
 export class InputLineFeedSeparattedSequenceStream extends TextSplitStream {
-  constructor(options?: Partial<TextSplitStreamOptions>) {
+  constructor(options?: InputLineFeedSeparattedSequenceStreamOptions) {
     let { splitter, chunkEndSplit } = options ?? {};
     splitter ??= LINEFEED_SEPARATOR;
     chunkEndSplit ??= false;
